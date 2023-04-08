a9a4164eca58c826b8b62f5495f8880072b076a6
tony <tony.dg.tg@gmail.com>
2023-04-08 12:38:05 +0200

    mobile version

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-const acceuil = document.querySelector(".Accueil");
-
-function isElementInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-function checkAnimation() {
-  if (isElementInViewport(acceuil)) {
-    acceuil.classList.add("animate");
-    window.removeEventListener("scroll", checkAnimation);
-  }
-}
-
-window.addEventListener("scroll", checkAnimation);
-checkAnimation();
-
-window.onload = function () {
-  document.querySelector(".accueil").style.opacity = 1;
-  document.querySelector(".accueil").style.transform = "translateY(0)";
-};
-
 const scrollTexts = document.querySelectorAll(".scroll-text");
 
 scrollTexts.forEach((scrollText) => {
@@ -36,5 +8,12 @@ scrollTexts.forEach((scrollText) => {
     if (scrollPosition > scrollTextPosition) {
       scrollText.classList.add("visible");
     }
+  });
+});
+
+const accordions = document.querySelectorAll(".accordion .arrow");
+accordions.forEach((accordion) => {
+  accordion.addEventListener("click", () => {
+    accordion.parentNode.parentNode.classList.toggle("open");
   });
 });
